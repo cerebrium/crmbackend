952a8d3595c178a976a4d707d044fcaea37b5bc0
cerebrium <nicholas.m.shankland@gmail.com>
2020-04-13 20:33:58 -0700

    changed page size

--- pythonicbackend/settings.py
+++ pythonicbackend/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 10000,
 }
 
 # Password validation
