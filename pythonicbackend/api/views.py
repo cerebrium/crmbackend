2678d3f4f3c75620d643b48fdc6b92f18ca3bcdf
cerebrium <nicholas.m.shankland@gmail.com>
2020-04-21 12:57:45 -0700

    wrote some functions

--- pythonicbackend/api/views.py
+++ pythonicbackend/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer, DriverSerializer, ScheduledDatesSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     """
     API endpoint that allows users to be viewed or edited.
     """
@@ -15,25 +15,51 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 class DriverViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,) 
+    # permission_classes = (IsAuthenticated,) 
 
     queryset = Driver.objects.all().order_by('name')
     serializer_class = DriverSerializer
 
 class ScheduleViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     """
     API endpoint that allows drivers to be viewed or edited
     """
-    queryset = ScheduledDate.objects.all()
+    queryset = ScheduledDate.objects.all().order_by('driver_id')
     serializer_class = ScheduledDatesSerializer
 
-class CoolViewSet(APIView):
-    permission_classes = (IsAuthenticated,)    
+class DataViewSet(APIView):
+    # permission_classes = (IsAuthenticated,)    
 
     def get(self, request):
+        # First returned array
+        # grab all of the objects stored in the drivers class
+        drivers = Driver.objects.all().order_by('name')
+
+        # make data type to store stuff in -- array in this case
+        myDriverArray = []
+
+        # loop through each object in the data class (drivers) and grab the data i want... push it into the array for display
+        for name in drivers:
+            myDriverArray.append(name.name)
+
+        #######################################################################################################################################    
+        # Second returned array
+
+        # grab all of the objects stored in the schedule class
+        schedule = ScheduledDate.objects.all().order_by('driver_id')
+        
+        # make another array to store the dates data i want
+        myScheduleArray = []
+
+        # Loop through each object in this array and do some math on it, push the value to the array for display
+        for dateObject in schedule:
+            myScheduleArray.append(dateObject.LWP + dateObject.LVP)
+
+
         content = {
-            'message': 'hello there'
+            'drivers_names': myDriverArray,
+            'LWP_and_LVP': myScheduleArray
         }
         return Response(content)
 
