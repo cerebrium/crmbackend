1d7f4d1450f991561d41c0095e4e480df8f6fea4
cerebrium <nicholas.m.shankland@gmail.com>
2020-08-02 12:49:07 -0700

    some changes

--- pythonicbackend/settings.py
+++ pythonicbackend/settings.py
@@ -101,16 +101,28 @@ WSGI_APPLICATION = 'pythonicbackend.wsgi.application'
 #     }
 # else:    
     # Nikitch Local Database Configuration
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crmbackend',
-        'USER': 'postgres',
-        'PASSWORD': 'Ginishka04121995',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'crmbackend',
+            'USER': 'postgres',
+            'PASSWORD': 'Ginishka04121995',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 # Ginishka local database Configuration
 # DATABASES = {
 #     'default': {
